9287cb98c96ab85b974b850913ed0d74230a7504
枫枫 <guozhiyong@range-soft.com>
2025-06-13 13:49:45 +0800

    接入方糖sdk

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module flame_clouds
 go 1.24.0
 
 require (
+	github.com/easychen/serverchan-sdk-golang v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- service/sct_service/enter.go
+++ service/sct_service/enter.go
@@ -1,14 +1,11 @@
 package sct_service
 
 import (
-	"bytes"
-	"encoding/json"
 	"flame_clouds/config"
 	"flame_clouds/global"
 	"fmt"
+	serverchan_sdk "github.com/easychen/serverchan-sdk-golang"
 	"github.com/sirupsen/logrus"
-	"io"
-	"net/http"
 	"strings"
 )
 
@@ -29,53 +26,19 @@ func SendServerNotification(notification AlertNotification) {
 	)
 	message = strings.ReplaceAll(message, "<br>", "")
 
-	// 如果有图片链接，添加到消息中
-	//if notification.ImageURL != "" {
-	//	message += fmt.Sprintf("  ![火烧云预测](%s)", notification.ImageURL)
-	//}
-
 	if !global.Config.Bot.Enable {
 		logrus.Infof("未配置消息推送渠道 %s", message)
 		return
 	}
 	logrus.Infof(message)
 
-	type FtReq struct {
-		Title string `json:"title"`
-		Desp  string `json:"desp"`
-	}
-	byteData, _ := json.Marshal(FtReq{
-		Title: notification.Event.EventType.String() + "火烧云预警",
-		Desp:  message,
-	})
-	req, _ := http.NewRequest("POST", fmt.Sprintf("https://sctapi.ftqq.com/%s.send", global.Config.Bot.SendKey), bytes.NewReader(byteData))
-	req.Header.Set("Content-Type", "application/json;charset=utf-8")
+	title := notification.Event.EventType.String() + "火烧云预警"
 
-	res, err := http.DefaultClient.Do(req)
+	resp, err := serverchan_sdk.ScSend(global.Config.Bot.SendKey, title, message, nil)
 	if err != nil {
 		logrus.Errorf("消息推送失败 %s", err)
 		return
 	}
-	byteData, _ = io.ReadAll(res.Body)
-
-	type ServerType struct {
-		Code    int    `json:"code"`
-		Message string `json:"message"`
-		Data    struct {
-			Pushid  string `json:"pushid"`
-			Readkey string `json:"readkey"`
-			Error   string `json:"error"`
-			Errno   int    `json:"errno"`
-		} `json:"data"`
-	}
-
-	var t ServerType
-	err = json.Unmarshal(byteData, &t)
-	if err != nil {
-		logrus.Errorf("%s json解析失败, %s", string(byteData), err)
-		return
-	}
-
-	logrus.Infof("推送成功 pushID:%s", t.Data.Pushid)
+	logrus.Infof("消息推送成功 %#v", resp)
 	return
 }
